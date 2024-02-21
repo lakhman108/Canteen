9b94063cd5012c74f68fa1e26d22ba9b6c201162
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2024-02-21 11:31:36 +0000

    Add model registrations to admin.py

--- canteen/admin.py
+++ canteen/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CustomUser, Orders, OrderDetails, Payment, Food, FoodDetails
+
+admin.site.register(CustomUser)
+admin.site.register(Orders)
+admin.site.register(OrderDetails)
+admin.site.register(Payment)
+admin.site.register(Food)
+admin.site.register(FoodDetails)
