dbc0085dbdaccc997fe097534c185f64e56dea20
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2024-03-02 18:02:25 +0000

    fixing login issue

--- example/views.py
+++ example/views.py
@@ -1,37 +1,31 @@
-
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect ,HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import CustomUserLoginForm, CustomUserRegistrationForm
 from canteen.models import CustomUser
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
+from database.models import News
 
+@login_required
 def contact(request):
-    return HttpResponse("<h1>m name is lakhman</h1>")
+   
 
 @login_required
 def index(request):
-
-    # newsdata = News.objects.all()
-    # news_data = []
-    # for data in newsdata:
-    #     print(data.title)
-    #     print(data.content)
-    #     print(data.created_at)
-    #     print(data.updated_at)
-    #     print(data.is_published)
-    #     news_data.append({
-    #         'title': data.title,
-    #         'content': data.content,
-    #         'created_at': data.created_at,
-    #         'updated_at': data.updated_at,
-    #         'is_published': data.is_published
-    #     })
-  
-    return render(request, 'index.html')
-
+    newsdata = News.objects.all()
+    news_data = []
+    for data in newsdata:
+        news_data.append({
+            'title': data.title,
+            'content': data.content,
+            'created_at': data.created_at,
+            'updated_at': data.updated_at,
+            'is_published': data.is_published
+        })
+    
+    return render(request, 'index.html', {'news_data': news_data})
 
 def custom_user_login(request):
     if request.method == 'POST':
@@ -47,23 +41,22 @@ def custom_user_login(request):
                 # Log in the user
                 login(request, user)
                 return redirect('example:index')
-
+            else:
+                messages.error(request, 'Invalid username or password.')
+        else:
+            messages.error(request, 'Invalid form data.')
     else:
         form = CustomUserLoginForm()
 
     return render(request, 'login.html', {'form': form})
 
-
-
-    
-    
 def custom_user_register(request):
     if request.method == 'POST':
         form = CustomUserRegistrationForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
-            email = "default@gmail.com"  # Provide a default email or get it from the form
+            email = username+"@gmail.com"  # Get email from the form
 
             # Create the user using create_user method
             user = User.objects.create_user(username, email, password)
@@ -72,6 +65,8 @@ def custom_user_register(request):
             login(request, user)
 
             return redirect('example:index')
+        else:
+            messages.error(request, 'Invalid form data.')
     else:
         form = CustomUserRegistrationForm()
 
--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -57,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+   
+     
 ]
 
 ROOT_URLCONF = 'vercel_app.urls'
