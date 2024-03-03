4ccb16c022b250f918f416c8060a196ff9fac634
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2024-03-03 04:49:01 +0000

    indices changed

--- vercel_app/urls.py
+++ vercel_app/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('example/', include('example.urls',namespace='example')),
+    path('', include('example.urls',namespace='example')),
 ]
 
 # add at the last
