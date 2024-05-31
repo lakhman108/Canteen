258e3dcab3b1c52aa60519fd880dfe16ca1d427b
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2024-05-31 16:12:35 +0530

    wsgi changes

--- vercel_app/wsgi.py
+++ vercel_app/wsgi.py
@@ -1,26 +1,15 @@
 """
 WSGI config for vercel_app project.
 
-It exposes the WSGI callable as a module-level variable named ``app``.
+It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_app.settings')
 
-app = get_wsgi_application()
-
-
-
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saveplate.settings')
-# application = get_wsgi_application()
-# app = application #add here
+application = get_wsgi_application()
