60058ae84240f989ec036d4954ee6a46a33f0c99
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2024-05-31 15:40:05 +0530

    some changes

--- canteen/models.py
+++ canteen/models.py
@@ -43,7 +43,7 @@ class FoodDetails(models.Model):
     stock_qty = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     photo_url = models.CharField(max_length=255, default=None)
-    rating=models.IntegerField(default=0,blank=True,null=True,max_length=1)
+    rating=models.IntegerField(default=0,blank=True,null=True)
 
     def __str__(self):
         return self.name
